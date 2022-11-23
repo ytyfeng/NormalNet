b06c1ee8545bb7ec858dab3421792700b76e39b8
Ty Feng <ty@radiancelabs.com>
2022-11-23 09:39:36 -0800

    add pointnet

---
+++ pointnet.pytorch/scripts/build.sh
@@ -0,0 +1,5 @@
+
+SCRIPTPATH=`pwd`
+echo $SCRIPTPATH
+
+g++ -std=c++11 $SCRIPTPATH/../utils/render_balls_so.cpp -o $SCRIPTPATH/../utils/render_balls_so.so -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
