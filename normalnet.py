73095d28b7a3d813d1afc6607f50a8c85b2b9d2d
Ty Feng <ty@radiancelabs.com>
2022-12-06 01:58:43 -0800

    input transform fix

--- normalnet.py
+++ normalnet.py
@@ -93,11 +93,11 @@ class STN(nn.Module):
 class PointNetfeat(nn.Module):
     def __init__(self, num_points=500, feature_transform=True, sym_op='max'):
         super(PointNetfeat, self).__init__()
-        self.stn3d = STN3d()
+        #self.stn3d = STN3d()
         self.num_points = num_points
         self.feature_transform = feature_transform
         self.sym_op = sym_op
-        
+        self.stn1 = QSTN(num_points=num_points, dim=3, sym_op=self.sym_op)
         if self.feature_transform:
             self.stn = STN(num_points=num_points, dim=64, sym_op=self.sym_op)
 
@@ -119,10 +119,14 @@ class PointNetfeat(nn.Module):
         
     def forward(self, x):
         n_pts = x.size()[2]
-        trans = self.stn3d(x)
+        # input transform
+        # from tuples to list of single points
+        x = x.view(x.size(0), 3, -1)
+        trans = self.stn1(x)
         x = x.transpose(2, 1)
         x = torch.bmm(x, trans)
         x = x.transpose(2, 1)
+        x = x.contiguous().view(x.size(0), 3, -1)
 
         # mlp (64,64)
         x = F.relu(self.bn0a(self.conv0a(x)))
@@ -198,3 +202,75 @@ class NormalNet(nn.Module):
 
         return x, trans, trans_feat
 
+class QSTN(nn.Module):
+    def __init__(self, num_points=500, dim=3, sym_op='max'):
+        super(QSTN, self).__init__()
+
+        self.dim = dim
+        self.sym_op = sym_op
+        self.num_points = num_points
+
+        self.conv1 = torch.nn.Conv1d(self.dim, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.mp1 = torch.nn.MaxPool1d(num_points)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 4)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        # symmetric operation over all points
+        x = self.mp1(x)
+        x = x.view(-1, 1024*self.num_scales)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        # add identity quaternion (so the network can output 0 to leave the point cloud identical)
+        iden = x.new_tensor([1, 0, 0, 0])
+        x = x + iden
+
+        # convert quaternion to rotation matrix
+        x = batch_quat_to_rotmat(x)
+
+        return x
+
+# quaternion a + bi + cj + dk should be given in the form [a,b,c,d]
+def batch_quat_to_rotmat(q, out=None):
+
+    batchsize = q.size(0)
+
+    if out is None:
+        out = q.new_empty(batchsize, 3, 3)
+
+    # 2 / squared quaternion 2-norm
+    s = 2/torch.sum(q.pow(2), 1)
+
+    # coefficients of the Hamilton product of the quaternion with itself
+    h = torch.bmm(q.unsqueeze(2), q.unsqueeze(1))
+
+    out[:, 0, 0] = 1 - (h[:, 2, 2] + h[:, 3, 3]).mul(s)
+    out[:, 0, 1] = (h[:, 1, 2] - h[:, 3, 0]).mul(s)
+    out[:, 0, 2] = (h[:, 1, 3] + h[:, 2, 0]).mul(s)
+
+    out[:, 1, 0] = (h[:, 1, 2] + h[:, 3, 0]).mul(s)
+    out[:, 1, 1] = 1 - (h[:, 1, 1] + h[:, 3, 3]).mul(s)
+    out[:, 1, 2] = (h[:, 2, 3] - h[:, 1, 0]).mul(s)
+
+    out[:, 2, 0] = (h[:, 1, 3] - h[:, 2, 0]).mul(s)
+    out[:, 2, 1] = (h[:, 2, 3] + h[:, 1, 0]).mul(s)
+    out[:, 2, 2] = 1 - (h[:, 1, 1] + h[:, 2, 2]).mul(s)
+
+    return out
